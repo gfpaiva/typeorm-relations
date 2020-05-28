5252f291e3cc3ffb9aa6048b0431b85e32e081ef
guilherme.paiva <guilherme.paiva@luizalabs.com>
2020-05-28 18:46:03 -0300

    feat: :sparkles: add migrations, customers module and products module

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -6,20 +6,31 @@ import {
   OneToOne,
   JoinColumn,
   OneToMany,
+  Column,
 } from 'typeorm';
 
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('uuid')
+  customer_id: string;
+
+  @OneToOne(() => Customer)
+  @JoinColumn()
   customer: Customer;
 
+  @OneToMany(() => OrdersProducts, ordersProducts => ordersProducts.order)
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
@@ -11,23 +11,34 @@ import {
 import Order from '@modules/orders/infra/typeorm/entities/Order';
 import Product from '@modules/products/infra/typeorm/entities/Product';
 
+@Entity('orders_products')
 class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ManyToOne(() => Order, order => order.order_products)
   order: Order;
 
+  @ManyToOne(() => Product, product => product.order_products)
   product: Product;
 
+  @Column('uuid')
   product_id: string;
 
+  @Column('uuid')
   order_id: string;
 
+  @ManyToOne(() => Product, product => product.price, { cascade: ['insert'] })
+  @JoinColumn({ name: 'price' })
   price: number;
 
+  @Column('integer')
   quantity: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
